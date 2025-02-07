994f1335d158f761ba829ce3d614ccb376a45c04
lucaszstrr <lucaaszstr@gmail.com>
2025-02-07 08:32:32 -0300

    Exercício área do losango

---
+++ exercicio_g/index.php
@@ -0,0 +1,14 @@
+<?php
+
+    $diagMenor = 10;
+    $diagMaior = 20;
+
+    echo "A diagonal menor é: $diagMenor";
+    echo "<br>";
+    echo "A diagonal maior é: $diagMaior";
+    echo "<br>";
+    echo "<br>";
+
+    $area = ($diagMenor * $diagMaior) / 2;
+    echo "A área do losango é: $area";
+ 
