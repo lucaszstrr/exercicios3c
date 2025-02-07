382741aea5da85ce6c2c37e5672f32066a191378
lucaszstrr <lucaaszstr@gmail.com>
2025-02-07 08:28:26 -0300

    Exercício da área do triângulo

---
+++ exercicio_f/index.php
@@ -0,0 +1,13 @@
+<?php 
+
+    $base = 30;
+    $altura = 20;
+
+    echo "A base é: $base";
+    echo "<br>";
+    echo "A altura é: $altura";
+    echo "<br>";
+    echo "<br>";
+
+    $area = ($base * $altura)/ 2;
+    echo "A área do triângulo é: $area";
